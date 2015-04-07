bf95023d64bf22a2e00053852aedd1b3d3c0e649
DivyaSaini <divya.bitsgoa@gmail.com>
2015-04-07 16:06:50 +0530

    Create EDAW1.R

---
+++ EDAW1.R
@@ -0,0 +1,17 @@
+### Exploratory Data Analysis
+# Principle 1: Show comparisons - a 'is relative to' b
+# Principle 2: Show causality, mechanism, explanation, systematic structure
+# Principle 3: Show multivariate data - more than 2 variables
+# Principle 4: Integration of evidence - words, numbers, images, diagrams 
+# Principle 5: Describe and document the evidence with appropriate labels, 
+#              scales, sources, etc.
+# Principle 6: Content is king
+
+## Why Exploratory Graphs?
+# R1. To understand data properties
+# R2. To find patterns in data
+# R3. To suggest modeling strategies
+# R4. To "debug" analyses
+# 5. To communicate results 
+
+setwd("C:/Users/disaini/Desktop/CourseraWD")
